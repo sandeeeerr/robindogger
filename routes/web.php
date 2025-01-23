d0f54732d1dde6bff260be9ed257648a36281dfa
Sander <sander@concept7.nl>
2025-01-23 22:16:26 +0100

    curator bug

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', Home::class)->name('home');
 Route::get('/article/{post:slug}', PostShow::class)->name('post.show');
+Route::get('/curator/test', function () {
+    return 'Curator route works!';
+});
